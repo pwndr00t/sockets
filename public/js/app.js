c0843ec233830462a30c80ca6b9d3818b794c384
Melih Değiş <melihdegis@gmail.com>
2016-02-07 01:48:14 +0200

    front end added

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,16 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
     console.log('New Message:');
     console.log(message.text);
-})
+    jQuery(".messages").append("<p>" + message.text +"<p>");
+});
+
+// handles submiting of new message
+var $form = jQuery('#message-form');
+var $msg = $form.find('input[name=message]');
+$form.on('submit', function (event) {
+    event.preventDefault();
+    socket.emit('message', {
+        text: $msg.val()
+    });
+    $msg.val('');
+});
